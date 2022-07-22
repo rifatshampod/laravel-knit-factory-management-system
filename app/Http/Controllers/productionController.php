61f7ec34adb10da0beca68319db620672ec7ae01
rifatshampod <shampodrifat@gmail.com>
2022-07-23 03:38:57 +0800

    total production adding from daily production data

--- app/Http/Controllers/productionController.php
+++ app/Http/Controllers/productionController.php
@@ -13,22 +13,23 @@ use Illuminate\Support\Facades\DB;
 class productionController extends Controller
 {
     function showData(Request $req){
-\DB::statement("SET SQL_MODE=''");
-$productionlist = Production::select('productions.id as id','orders.id as orderId','orders.artwork',
-'orders.style','orders.order_no','orders.body_color','orders.print_quality',
-'orders.parts_name','orders.print_color','orders.total_qty','plans.target_perday',
-'productions.inhand','productions.today_production',
-'productions.total_production','productions.balance','productions.status as productionStatus',
-DB::raw('SUM(daily_productions.today_production) AS total_today_production'))
-->join('plans','plans.id','=','productions.plan_id')
-->join('orders','orders.id','=','plans.order_id')
-->join('daily_productions','daily_productions.production_id','=','productions.id')
-->groupBy('daily_productions.production_id')
-->orderBy('productions.id', 'DESC')
-->get();
+        \DB::statement("SET SQL_MODE=''");
+        $productionlist = Production::select('productions.id as id','orders.id as orderId','orders.artwork',
+        'orders.style','orders.order_no','orders.body_color','orders.print_quality',
+        'orders.parts_name','orders.print_color','orders.total_qty','plans.target_perday',
+        'productions.inhand','productions.today_production',
+        'productions.total_production','productions.balance','productions.status as productionStatus',
+        DB::raw('SUM(daily_productions.today_production) AS total_today_production'),
+        DB::raw('SUM(daily_productions.balance) AS total_balance'))
+        ->join('plans','plans.id','=','productions.plan_id')
+        ->join('orders','orders.id','=','plans.order_id')
+        ->leftjoin('daily_productions','daily_productions.production_id','=','productions.id')
+        ->groupBy('daily_productions.production_id')
+        ->orderBy('productions.id', 'DESC')
+        ->get();
 
-return view('allProduction',['productionlist'=>$productionlist]);
-}
+        return view('allProduction',['productionlist'=>$productionlist]);
+    }
 
 
 
